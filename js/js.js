dd4cfcb38adedc6b87877adaf308f17aeeaee45d
Yuri Silva <yuricosta149@gmail.com>
2021-09-09 18:56:05 -0300

    First Commit

---
+++ js/js.js
@@ -0,0 +1,11 @@
+// function exibirMensagem() {
+//     window.alert("Olá, mundo!")
+// }
+
+// exibirMensagem()
+
+var fruits = ["Apple", "Orange", "Plum"]
+
+for (var fruit of fruits) {
+    alert(fruit)
+}
